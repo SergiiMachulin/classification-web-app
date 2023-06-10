f9e7e32e0ba41bd150cea73d79577407135f4917
Sergii Machulin <machulinsv@gmail.com>
2023-06-10 14:12:19 +0300

    Refactor code and error handling

--- app.py
+++ app.py
@@ -1,38 +1,59 @@
 import os
 import urllib
 import uuid
+import logging
 
 from PIL import Image
 from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.utils import load_img, img_to_array
+from urllib.error import URLError
+from werkzeug.utils import secure_filename
 
 app = Flask("ClassificationWebApp")
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model = load_model(os.path.join(BASE_DIR, "modeling/model.hdf5"))
+MODEL = load_model(os.path.join(BASE_DIR, "modeling/model.hdf5"))
+TARGET_IMG = os.path.join(BASE_DIR, "static/images")
+ALLOWED_EXT: set = {"jpg", "jpeg", "png", "jfif"}
 
-ALLOWED_EXT = {"jpg", "jpeg", "png", "jfif"}
 
-
-def allowed_file(filename):
+def allowed_file(filename: str) -> bool:
     return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXT
 
 
-classes = [
-    "airplane",
-    "automobile",
-    "bird",
-    "cat",
-    "deer",
-    "dog",
-    "frog",
-    "horse",
-    "ship",
-    "truck",
-]
+def handle_url_upload(link: str) -> tuple[str, str]:
+    try:
+        resource = urllib.request.urlopen(link)
+        unique_filename = str(uuid.uuid4())
+        filename = unique_filename + ".jpg"
+        img_path = os.path.join(TARGET_IMG, filename)
+        output = open(img_path, "wb")
+        output.write(resource.read())
+        output.close()
+
+        return img_path, ""
+
+    except (URLError, ValueError) as e:
+        logging.error(str(e))
+        error = "Invalid URL or the image is not accessible"
+        return "", error
 
 
-def predict(filename, model):
+def handle_file_upload(file) -> tuple[str, str]:
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(TARGET_IMG, filename))
+        img_path = os.path.join(TARGET_IMG, filename)
+
+        return img_path, ""
+
+    else:
+        error = "Please upload images of jpg, jpeg, and png extensions only"
+        return "", error
+
+
+def predict(filename: str, model) -> tuple[list[str], list[float]]:
     img = load_img(filename, target_size=(32, 32))
     img = img_to_array(img)
     img = img.reshape(1, 32, 32, 3)
@@ -41,8 +62,21 @@ def predict(filename, model):
     img = img / 255.0
     result = model.predict(img)
 
+    classes = [
+        "airplane",
+        "automobile",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    ]
+
     dict_result = {}
-    for i in range(10):
+    for i in range(len(classes)):
         dict_result[result[0][i]] = classes[i]
 
     res = result[0]
@@ -57,77 +91,53 @@ def predict(filename, model):
 
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def home() -> str:
+    return render_template("index.html", error="")
 
 
 @app.route("/success", methods=["GET", "POST"])
-def success():
+def success() -> str:
     error = ""
-    target_img = os.path.join(os.getcwd(), "static/images")
+
     if request.method == "POST":
         if request.form:
             link = request.form.get("link")
-            try:
-                resource = urllib.request.urlopen(link)
-                unique_filename = str(uuid.uuid4())
-                filename = unique_filename + ".jpg"
-                img_path = os.path.join(target_img, filename)
-                output = open(img_path, "wb")
-                output.write(resource.read())
-                output.close()
-                img = filename
-
-                class_result, prob_result = predict(img_path, model)
-
-                predictions = {
-                    "class1": class_result[0],
-                    "prob1": prob_result[0],
-                }
-
-            except Exception as e:
-                print(str(e))
-                error = (
-                    "This image from this site is not accesible or "
-                    "inappropriate input"
-                )
-
-            if len(error) == 0:
-                return render_template(
-                    "success.html", img=img, predictions=predictions
-                )
+            if link.strip() != "":
+                img_path, error = handle_url_upload(link)
             else:
-                return render_template("index.html", error=error)
-
+                error = "Please enter a valid URL"
         elif request.files:
             file = request.files["file"]
-            if file and allowed_file(file.filename):
-                file.save(os.path.join(target_img, file.filename))
-                img_path = os.path.join(target_img, file.filename)
-                img = file.filename
+            img_path, error = handle_file_upload(file)
+        else:
+            error = "Invalid request"
 
-                class_result, prob_result = predict(img_path, model)
+        if error:
+            return render_template("index.html", error=error)
 
-                predictions = {
-                    "class1": class_result[0],
-                    "prob1": prob_result[0],
-                }
+        img = os.path.basename(img_path)
+        class_result, prob_result = predict(img_path, MODEL)
 
-            else:
-                error = (
-                    "Please upload images of jpg , jpeg and png extension only"
-                )
+        predictions = {
+            "class1": class_result[0],
+            "prob1": prob_result[0],
+        }
 
-            if len(error) == 0:
-                return render_template(
-                    "success.html", img=img, predictions=predictions
-                )
-            else:
-                return render_template("index.html", error=error)
+        return render_template(
+            "success.html", img=img, predictions=predictions, error=""
+        )
 
     else:
-        return render_template("index.html")
+        return render_template("index.html", error="")
+
+
+def run_flask_app() -> None:
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Configure logging
+    logging.basicConfig(filename="app.log", level=logging.ERROR)
+
+    # Start Flask app
+    run_flask_app()
